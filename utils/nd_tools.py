272b67bda6024904df2c83d3591e2d1a119fc1de
Charles Marshak <charlie.z.marshak@jpl.nasa.gov>
2019-12-05 15:45:45 -0800

    added typing and some docstrings

--- utils/nd_tools.py
+++ utils/nd_tools.py
@@ -4,6 +4,7 @@ from functools import wraps
 from scipy.ndimage import find_objects
 from scipy.signal import convolve2d
 from scipy.ndimage import measurements, find_objects
+from typing import Callable
 
 
 def fill_mask_with_constant_value(band_func=None, *, fill_value=0):
@@ -89,7 +90,8 @@ def get_array_from_features(label_array: np.ndarray,
     return out
 
 
-def get_features_from_array(label_array, matrix):
+def get_features_from_array(label_array: np.ndarray,
+                            array: np.ndarray) -> np.ndarray:
     """
     From single image and labels, obtain features with appropriate shape.
 
@@ -98,16 +100,16 @@ def get_features_from_array(label_array, matrix):
         get_features_from_array(l, get_array_from_features(l, f)) == f
     """
     # Ensure that 2d label_array has the same 1st two dimensions as matrix
-    assert(label_array.shape == (matrix.shape[0], matrix.shape[1]))
+    assert(label_array.shape == (array.shape[0], array.shape[1]))
     labels_p1 = label_array + 1
     indices = find_objects(labels_p1)
     labels_unique = np.unique(labels_p1)
 
     m = len(labels_unique)
-    if len(matrix.shape) == 2:
+    if len(array.shape) == 2:
         features = np.zeros((m, 1))
-    elif len(matrix.shape) == 3:
-        features = np.zeros((m, matrix.shape[2])).astype(bool)
+    elif len(array.shape) == 3:
+        features = np.zeros((m, array.shape[2])).astype(bool)
     else:
         raise ValueError('Matrix must be 2d or 3d')
 
@@ -116,22 +118,34 @@ def get_features_from_array(label_array, matrix):
         # if features is m x 1, then do not need extra dimension when indexing
         if features.shape[1] == 1:
             # import pdb; pdb.set_trace()
-            features[k, 0] = matrix[indices_temp][labels_p1[indices_temp] == label][0]
+            features[k, 0] = array[indices_temp][labels_p1[indices_temp] == label][0]
         # if features is m x n with n > 1, then requires extra dimension when indexing
         else:
-            features[k, ...] = matrix[indices_temp + (np.s_[:], )][labels_p1[indices_temp] == label][0, ...]
+            features[k, ...] = array[indices_temp + (np.s_[:], )][labels_p1[indices_temp] == label][0, ...]
     return features
 
 
-def get_superpixel_means_band(label_array, band):
-    # Assume labels are 0, 1, 2, ..., n
+def get_superpixel_means_band(label_array: np.ndarray,
+                              band: np.ndarray) -> np.ndarray:
+    """Assume labels in label_array are 0, 1, 2, ..., n
+
+    Returns array of shape = (len(np.unique(labels)) x 1)
+    """
     labels_ = label_array + 1  # scipy wants labels to begin at 1 and transforms to 1, 2, ..., n+1
     labels_unique = np.unique(labels_)
     means = measurements.mean(band, labels=labels_, index=labels_unique)
     return means.reshape((-1, 1))
 
 
-def get_superpixel_means_as_features(label_array, img):
+def get_superpixel_means_as_features(label_array: np.ndarray,
+                                     img: np.ndarray) -> np.ndarray:
+    """Assume labels in label_array are 0, 1, 2, ..., n
+
+    Returns array of shape = (len(np.unique(labels)) x q), where q is number of channels,
+    meaning
+        + q = 1 if len(img.shape) == 2 or
+        + q = img.shape[2] if len(img.shape==3)
+    """
     if len(img.shape) == 2:
         measurements = get_superpixel_means_band(label_array, img)
     elif len(img.shape) == 3:
@@ -142,15 +156,27 @@ def get_superpixel_means_as_features(label_array, img):
     return measurements
 
 
-def get_superpixel_stds_band(label_array, band):
-    # Assume labels are 0, 1, 2, ..., n
+def get_superpixel_stds_band(label_array: np.ndarray,
+                             band: np.ndarray) -> np.ndarray:
+    """Assume labels in label_array are 0, 1, 2, ..., n
+
+    Returns array of shape = (len(np.unique(labels)) x 1)
+    """
     labels_ = label_array + 1  # scipy wants labels to begin at 1 and transforms to 1, 2, ..., n+1
     labels_unique = np.unique(labels_)
     means = measurements.standard_deviation(band, labels=labels_, index=labels_unique)
     return means.reshape((-1, 1))
 
 
-def get_superpixel_stds_as_features(label_array, img):
+def get_superpixel_stds_as_features(label_array: np.ndarray,
+                                    img: np.ndarray) -> np.ndarray:
+    """Assume labels in label_array are 0, 1, 2, ..., n
+
+    Returns array of shape = (len(np.unique(labels)) x q), where q is number of channels,
+    meaning
+        + q = 1 if len(img.shape) == 2 or
+        + q = img.shape[2] if len(img.shape==3)
+    """
     if len(img.shape) == 2:
         measurements = get_superpixel_stds_band(label_array, img)
     elif len(img.shape) == 3:
@@ -161,13 +187,18 @@ def get_superpixel_stds_as_features(label_array, img):
     return measurements
 
 
-def apply_func_to_superpixels(func,
+def apply_func_to_superpixels(func: Callable[[np.ndarray], np.ndarray],
                               labels: np.ndarray,
-                              array: np.ndarray):
+                              array: np.ndarray) -> np.ndarray:
     """
     This is a wrapper for `scipy.ndimage.labeled_comprehension`.
 
-    It applies the func to the array of pixels within each label and returns the measurements.
+    It applies the func to the array of pixels within each label and returns the measurements as features.
+
+    Returns array of shape = (len(np.unique(labels)) x q), where q is number of channels,
+    meaning
+        + q = 1 if len(img.shape) == 2 or
+        + q = img.shape[2] if len(img.shape==3)
     """
     if len(array.shape) != 2:
         raise ValueError('The array must be a 2d array')
@@ -177,16 +208,18 @@ def apply_func_to_superpixels(func,
     return new_change_measurements.reshape((-1, 1))
 
 
-def get_superpixel_area_as_features(labels):
+def get_superpixel_area_as_features(labels: np.ndarray) -> np.ndarray:
     return apply_func_to_superpixels(np.size, labels, labels).astype(int)
 
 
 @fill_mask_with_constant_value(fill_value=0)
-def filter_binary_array_by_min_size(binary_array,
-                                    min_size,
-                                    structure=np.ones((3, 3)),
-                                    mask=None):
-
+def filter_binary_array_by_min_size(binary_array: np.ndarray,
+                                    min_size: int,
+                                    structure: np.ndarray = np.ones((3, 3)),
+                                    mask: np.ndarray = None) -> np.ndarray:
+    """
+    Segments image into contiguous areas and then removes segments of size smaller than `min_size`.
+    """
     binary_array_temp = binary_array[~np.isnan(binary_array)]
     if ~((binary_array_temp == 0) | (binary_array_temp == 1)).all():
         raise ValueError('Array must be binary!')
@@ -200,7 +233,7 @@ def filter_binary_array_by_min_size(binary_array,
     binary_array_filtered = get_array_from_features(connected_component_labels, filtered_size_features)
     return binary_array_filtered
 
-def lee_filter_band(band, size, mask=None):
+def lee_filter_band(band: np.ndarray, size: int, mask: np.ndarray = None):
     """Source: https://stackoverflow.com/questions/39785970/speckle-lee-filter-in-python
     Reference for Updates: http://docs.astropy.org/en/stable/convolution/
 
@@ -224,7 +257,7 @@ def lee_filter_band(band, size, mask=None):
     return band_output
 
 
-def apply_2d_func_to_img(func,
+def apply_2d_func_to_img(func: Callable[[np.ndarray], np.ndarray],
                          img: np.ndarray,
                          *args,
                          **kwargs) -> np.ndarray:
@@ -241,20 +274,20 @@ def apply_2d_func_to_img(func,
         return new_img
 
 
-def lee_filter(img, size):
+def lee_filter(img: np.ndarray, size: np.ndarray) -> np.ndarray:
     return apply_2d_func_to_img(lee_filter_band, img, size)
 
 
 def scale_img(img: np.ndarray,
-              new_min=0,
-              new_max=1):
+              new_min: int = 0,
+              new_max: int = 1) -> np.ndarray:
     """
-    Scale an image by the absolute max and min in the array.
+    Scale an image by the absolute max and min in the array to have dynamic range new_min to new_max.
     """
     i_min = np.nanmin(img)
     i_max = np.nanmax(img)
     if i_min == i_max:
-        # then image is constant image and round to 0 or 1
-        return np.clip(img, 0, 1)
+        # then image is constant image and clip between new_min and new_max
+        return np.clip(img, new_min, new_max)
     img_scaled = (img - i_min) / (i_max - i_min) * (new_max - new_min) + new_min
     return img_scaled
